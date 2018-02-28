1468fcd179f0b04a1dcd084b72654c221d7431c5
kohnakagawa <tsunekou1019@gmail.com>
2018-02-28 14:54:18 +0900

    update

--- distance.sh
+++ distance.sh
@@ -20,7 +20,7 @@ if [ ! -e "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c3_c18.ndx" <<EOF
+gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c3_c18_sn1_tspc.ndx" <<EOF
 del 0-4
 a C33 | a C318
 r TSPC
@@ -29,7 +29,34 @@ del 0-1
 q
 EOF
 
-gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c18_c32.ndx" <<EOF
+gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c3_c18_sn2_tspc.ndx" <<EOF
+del 0-4
+a C23 | a C218
+r TSPC
+0 & 1
+del 0-1
+q
+EOF
+
+gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c3_c18_sn1_sopc.ndx" <<EOF
+del 0-4
+a C33 | a C318
+r SOPC
+0 & 1
+del 0-1
+q
+EOF
+
+gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c3_c18_sn2_sopc.ndx" <<EOF
+del 0-4
+a C23 | a C218
+r SOPC
+0 & 1
+del 0-1
+q
+EOF
+
+gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c18_c32_tspc.ndx" <<EOF
 del 0-4
 a C318 | a C332
 r TSPC
@@ -38,9 +65,35 @@ del 0-1
 q
 EOF
 
-gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c3_c18.ndx" -oav "${OUT_DIR}/c3_c18_aver.xvg" -oall "${OUT_DIR}/c3_c18.xvg" -oh "${OUT_DIR}/c3_c18_hist.xvg" <<EOF
+gmx make_ndx -f "${IN_DIR}/step7_1.gro" -o "${OUT_DIR}/c3_c32_tspc.ndx" <<EOF
+del 0-4
+a C33 | a C332
+r TSPC
+0 & 1
+del 0-1
+q
+EOF
+
+gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c3_c18_sn1_tspc.ndx" -oav "${OUT_DIR}/c3_c18_sn1_tspc_aver.xvg" -oall "${OUT_DIR}/c3_c18_sn1_tspc.xvg" -oh "${OUT_DIR}/c3_c18_sn1_tspc_hist.xvg" <<EOF
 0
 EOF
-gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c18_c32.ndx" -oav "${OUT_DIR}/c18_c32_aver.xvg" -oall "${OUT_DIR}/c18_c32.xvg" -oh "${OUT_DIR}/c18_c32_hist.xvg" <<EOF
+
+gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c3_c18_sn2_tspc.ndx" -oav "${OUT_DIR}/c3_c18_sn2_tspc_aver.xvg" -oall "${OUT_DIR}/c3_c18_sn2_tspc.xvg" -oh "${OUT_DIR}/c3_c18_sn2_tspc_hist.xvg" <<EOF
+0
+EOF
+
+gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c3_c18_sn1_sopc.ndx" -oav "${OUT_DIR}/c3_c18_sn1_sopc_aver.xvg" -oall "${OUT_DIR}/c3_c18_sn1_sopc.xvg" -oh "${OUT_DIR}/c3_c18_sn1_sopc_hist.xvg" <<EOF
+0
+EOF
+
+gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c3_c18_sn2_sopc.ndx" -oav "${OUT_DIR}/c3_c18_sn2_sopc_aver.xvg" -oall "${OUT_DIR}/c3_c18_sn2_sopc.xvg" -oh "${OUT_DIR}/c3_c18_sn2_sopc_hist.xvg" <<EOF
+0
+EOF
+
+gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c18_c32_tspc.ndx" -oav "${OUT_DIR}/c18_c32_tspc_aver.xvg" -oall "${OUT_DIR}/c18_c32_tspc.xvg" -oh "${OUT_DIR}/c18_c32_tspc_hist.xvg" <<EOF
+0
+EOF
+
+gmx distance -f "${IN_DIR}/merged_trr/merged_tot.trr" -s "${IN_DIR}/step7_1.tpr" -n "${OUT_DIR}/c3_c32_tspc.ndx" -oav "${OUT_DIR}/c3_c32_tspc_aver.xvg" -oall "${OUT_DIR}/c3_c32_tspc.xvg" -oh "${OUT_DIR}/c3_c32_tspc_hist.xvg" <<EOF
 0
 EOF
--- plot/density_plot.py
+++ plot/density_plot.py
@@ -21,16 +21,16 @@ def set_ticks(x_range, y_range):
     plt.yticks(np.arange(*y_range))
 
 
-def plot_carbon_density(data_file, carbon_name, lipid_name):
+def plot_carbon_density(data_file, label_name, lipid_name, factor):
     raw_data = np.loadtxt(data_file)
     z_cord = raw_data[:,0]
     density = raw_data[:,1]
     if lipid_name != "TSPC":
-        density /= 200.0
-    plt.plot(z_cord, density, '-', label=carbon_name)
+        density /= factor
+    plt.plot(z_cord, density, '-', label=label_name)
 
 
-def main(input_dir, legend_pos, lipids, carbons, poss):
+def main(input_dir, legend_pos, lipids, carbons, poss, fact):
     set_font()
     set_label("Relative position from membrane center [nm]", r"Number density [nm$^{-3}$]")
 
@@ -38,11 +38,10 @@ def main(input_dir, legend_pos, lipids, carbons, poss):
         carbon_names = ["-".join((l, c.replace("c3", "sn1-C").replace("c2", "sn2-C"))) for c in cs]
         fnames = [os.path.join(input_dir, "_".join((l, c, p)) + ".xvg") for c in cs]
         for fname, cname in zip(fnames, carbon_names):
-            plot_carbon_density(fname, cname, l)
+            plot_carbon_density(fname, cname, l, fact)
 
     plt.tight_layout()
     plt.legend(loc=legend_pos, fontsize=15)
-    # plt.legend(loc="upper right", fontsize=18)
     plt.savefig(os.path.join(input_dir, "{}_{}_density.eps".format("_".join(carbon_names), "_".join(poss))))
 
     # for check
@@ -55,6 +54,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--positions', nargs="+", dest="pos", required=True, help="leaflet positions [hi/lo/tot]")
     parser.add_argument('-c0', '--carbons0', nargs="+", dest='carbons0', required=True,  help="names of carbons0")
     parser.add_argument('-c1', '--carbons1', nargs="+", dest="carbons1", required=False, help="names of carbons1")
+    parser.add_argument('-f', '--normalize', dest="factor", required=True, help="normalized factor", type=int)
     parser.add_argument('-g', '--legend_pos', dest="legend_pos", required=False, default="upper right", help="legend position")
     args = parser.parse_args()
-    main(args.input_dir, args.legend_pos, args.lipids, (args.carbons0, args.carbons1), args.pos)
+    main(args.input_dir, args.legend_pos, args.lipids, (args.carbons0, args.carbons1), args.pos, args.factor)
